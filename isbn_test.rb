9aed6d5cdb43994abec74fb73ff915fd7b9eb71f
josephmckenzie <josephmckenzieaz@yahoo.com>
2015-09-21 18:50:29 -0400

    test for removing dashes from isbn number

--- isbn_test.rb
+++ isbn_test.rb
@@ -1,4 +1,6 @@
+#calls the file with the functions we are testing are looking to test
 require_relative"isbn.rb"
+#tells are program that we want minitest to automatically run when running this file
 require"minitest/autorun"
  
 class Isbn_verify<Minitest::Test  
@@ -9,6 +11,8 @@ assert_equal(false,verify_length("10293847570"))
 assert_equal(true,verify_length("0-321-14653-0"))
 end
 
-
+def test_removes_dashes_from_isbn
+assert_equal("0321146530",remove_dashes_from_isbn("0-321-14653-0"))
+end
 
 end
