ae1680f3a494cd79455e537370924c5eca8140aa
josephmckenzie <josephmckenzieaz@yahoo.com>
2015-09-21 18:46:48 -0400

    code added to check for 13 digits and to take out dashes from the isbn numbers

--- isbn.rb
+++ isbn.rb
@@ -1,13 +1,26 @@
-def isbn_checker(book_number)
-  verify_length
+def isbn_checker(isbn_number)
+#You want to call the dash remover function before verifying the length so it only counts numbers not dashes
+remove_dashes_from_isbn
+#verifies the length after removing the dashes from the isbn number
+verify_length
 end
 
-#this function verifies if the length of the isbn is a correct length 
+#this function verifies if the length of the isbn is a correct length of either 10 or 13
 
 def verify_length(isbn_number)
   if isbn_number.length==10
     true
+  elsif isbn_number.length==13
+  true
   else
     false
   end
 end
+
+
+#function for removing dashes from the isbn number 
+def remove_dashes_from_isbn(isbn_number)
+  if isbn_number.include?"-"
+	isbn_number.delete!"-"
+  end
+  end
--- isbn_test.rb
+++ isbn_test.rb
@@ -3,9 +3,12 @@ require"minitest/autorun"
  
 class Isbn_verify<Minitest::Test  
 
-def test_for_ten_digits
+def test_for_valid_amount_of_digits
 assert_equal(true,verify_length("2471958697"))
 assert_equal(false,verify_length("10293847570"))
 assert_equal(true,verify_length("0-321-14653-0"))
 end
+
+
+
 end
